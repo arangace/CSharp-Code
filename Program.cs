48bcdd1286c72518ba00d18847fa8612dfec6849
arangace <ahua291@aucklanduni.ac.nz>
2021-11-12 18:14:24 +1300

    working word conversion from one to ten

--- Program.cs
+++ Program.cs
@@ -1,34 +1,49 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 namespace unleashed
 {
     class Program
     {
+        static void convert(Dictionary<string, string> integers, string input)
+        {
+            if (integers.ContainsKey(input))
+            {
+                Console.WriteLine(integers[input]);
+            }
+            Console.WriteLine(input);
+            // Console.WriteLine(integers[number[i]]);
+        }
         static void Main(string[] args)
         {
-            var integers = new Dictionary<integer, string>(){
-{1, "one"},
-{2,"two"},
-{3, "three"},
-{4,"four"},
-{5,"five"},
-{6,"six"},
-{7,"seven"},
-{8,"eight"},
-{9,"nine"},
-{10,"ten"},
-            };
             Console.WriteLine("Input number to convert: ");
             string input = Console.ReadLine();
             Console.WriteLine("Your inputted number is " + input);
             try
             {
-                //23.4 two three dot four  
+                var integers = new Dictionary<string, string>(){
+                {"1", "one"},
+                {"2","two"},
+                {"3", "three"},
+                {"4","four"},
+                {"5","five"},
+                {"6","six"},
+                {"7","seven"},
+                {"8","eight"},
+                {"9","nine"},
+                {"10","ten"},
+
+                };
                 int number = Int32.Parse(input);
+
                 for (int i = 0; i < input.Length; i++)
                 {
-                    Console.WriteLine(input[i]);
+                    // Console.WriteLine(input[i]);
+
+                    convert(integers, input.Substring(i, 1));
+
                 }
+
             }
             // catch (IOException e)
             // {
@@ -43,5 +58,6 @@ namespace unleashed
                 Console.WriteLine("Exiting..");
             }
         }
+
     }
 }
