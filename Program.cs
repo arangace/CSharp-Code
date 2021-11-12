ec394d504de7d8fd3ec7b68e524ccc8a21d0f33f
arangace <ahua291@aucklanduni.ac.nz>
2021-11-12 17:03:50 +1300

    first working instance of code, reading user input

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+namespace unleashed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var integers = new Dictionary<integer, string>(){
+{1, "one"},
+{2,"two"},
+{3, "three"},
+{4,"four"},
+{5,"five"},
+{6,"six"},
+{7,"seven"},
+{8,"eight"},
+{9,"nine"},
+{10,"ten"},
+            };
+            Console.WriteLine("Input number to convert: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Your inputted number is " + input);
+            try
+            {
+                //23.4 two three dot four  
+                int number = Int32.Parse(input);
+                for (int i = 0; i < input.Length; i++)
+                {
+                    Console.WriteLine(input[i]);
+                }
+            }
+            // catch (IOException e)
+            // {
+            //     Console.WriteLine(e);
+            // }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Exiting..");
+            }
+        }
+    }
+}
