483a910d4d1c2888a7a331677133a31608ad3938
arangace <ahua291@aucklanduni.ac.nz>
2021-11-12 21:09:57 +1300

    working code for the first tens digits return correct wording

--- Program.cs
+++ Program.cs
@@ -5,13 +5,60 @@ namespace unleashed
 {
     class Program
     {
-        static void convert(Dictionary<string, string> integers, string input)
+        static void convert(string input, int currentUnit)
         {
+            var integers = new Dictionary<string, string>(){
+                {"00","zero"},
+                {"01", "one"},
+                {"02","two"},
+                {"03", "three"},
+                {"04","four"},
+                {"05","five"},
+                {"06","six"},
+                {"07","seven"},
+                {"08","eight"},
+                {"09","nine"},
+                {"10","ten"},
+                {"11","eleven"},
+                {"12","twleve"},
+                {"13","thirteen"},
+                {"14","fourteen"},
+                {"15","fifteen"},
+                {"16","sixteen"},
+                {"17","seventeen"},
+                {"18","eighteen"},
+                {"19","nineteen"},
+                };
+            var largeIntegers = new Dictionary<string, string>(){
+                {"2","twenty"},
+                {"3", "thirty"},
+                {"4","forty"},
+                {"5", "fifty"},
+                {"6","sixty"},
+                {"7","seventy"},
+                {"8","eighty"},
+                {"9","ninety"},
+                };
+            //numbers onwards sort
+            //checks against the dictionary for relative name
+            //Checks if the value is within 0 to 19
+            Console.WriteLine(input);
             if (integers.ContainsKey(input))
             {
                 Console.WriteLine(integers[input]);
+                Console.WriteLine(currentUnit);
             }
-            Console.WriteLine(input);
+            else if (largeIntegers.ContainsKey(input))
+            {
+                Console.WriteLine(largeIntegers[input]);
+                Console.WriteLine(currentUnit);
+            }
+            // else if (largeIntegers.ContainsKey(input.Substring(0, 1)))
+            // {
+            //     Console.WriteLine("i hit it");
+            //     Console.WriteLine(largeIntegers[input]);
+            //     Console.WriteLine(currentUnit);
+            // }
             // Console.WriteLine(integers[number[i]]);
         }
         static void Main(string[] args)
@@ -21,27 +68,24 @@ namespace unleashed
             Console.WriteLine("Your inputted number is " + input);
             try
             {
-                var integers = new Dictionary<string, string>(){
-                {"1", "one"},
-                {"2","two"},
-                {"3", "three"},
-                {"4","four"},
-                {"5","five"},
-                {"6","six"},
-                {"7","seven"},
-                {"8","eight"},
-                {"9","nine"},
-                {"10","ten"},
 
-                };
-                int number = Int32.Parse(input);
-
-                for (int i = 0; i < input.Length; i++)
+                for (int i = input.Length; i-- > 0;)
                 {
                     // Console.WriteLine(input[i]);
+                    //check if last two digits is between 0 and 19
+                    Console.WriteLine(i);
+                    if (i == input.Length - 1)
+                    {
 
-                    convert(integers, input.Substring(i, 1));
+                        Console.WriteLine("numbers is" + (input.Substring(i - 1, i - 1) + input.Substring(i, i - 1)));
+                        convert((input.Substring(i - 1, i - 1) + input.Substring(i, i - 1)), i);
 
+                    }
+                    else
+                    {
+
+                    }
+                    //parse it for digits between 20 to 99
                 }
 
             }
