e3c68d739d903d2c1cf6b552bdeab86cdae58d3b
arangace <ahua291@aucklanduni.ac.nz>
2021-11-12 22:48:48 +1300

    working functionality for all numbers with added changes for single digits

--- Program.cs
+++ Program.cs
@@ -8,19 +8,19 @@ namespace unleashed
         static void convert(string input)
         {
             var integers = new Dictionary<string, string>(){
-                {"00","zero"},
-                {"01", "one"},
-                {"02","two"},
-                {"03", "three"},
-                {"04","four"},
-                {"05","five"},
-                {"06","six"},
-                {"07","seven"},
-                {"08","eight"},
-                {"09","nine"},
+                {"0","zero"},
+                {"1", "one"},
+                {"2","two"},
+                {"3", "three"},
+                {"4","four"},
+                {"5","five"},
+                {"6","six"},
+                {"7","seven"},
+                {"8","eight"},
+                {"9","nine"},
                 {"10","ten"},
                 {"11","eleven"},
-                {"12","twleve"},
+                {"12","twelve"},
                 {"13","thirteen"},
                 {"14","fourteen"},
                 {"15","fifteen"},
@@ -51,7 +51,6 @@ namespace unleashed
             //else check if its value from 20 to 99
             else if (largeIntegers.ContainsKey(input.Substring(0, 1)))
             {
-                Console.WriteLine("i hit it");
                 Console.WriteLine(largeIntegers[input.Substring(0, 1)]);
             }
             // Console.WriteLine(integers[number[i]]);
@@ -68,32 +67,20 @@ namespace unleashed
                 Console.WriteLine(input.Length);
                 if (input.Length == 1)
                 {
-                    convert("0" + input);
+                    convert(input);
                 }
                 else
                 {
-                    //else if the value is two digits but between 0 and 19
+                    //else if the value is two digits and but between 20 and 99 returns the tens value
                     Console.WriteLine("substring" + input.Substring(input.Length - 2));
                     //Console.WriteLine("numbers is" + (input.Substring(input.Length - 1, input.Length - 2) + input.Substring(input.Length, input.Length - 1)));
                     convert(input.Substring(input.Length - 2));
                 }
 
-                for (int i = input.Length; i-- > 0;)
+                for (int i = input.Length - 2; i-- > 0;)
                 {
-                    // Console.WriteLine(input[i]);
-                    //check if last two digits is between 0 and 19
-                    Console.WriteLine(i);
-                    if (i == input.Length - 1)
-                    {
 
-
-
-                    }
-                    else
-                    {
-
-                    }
-                    //parse it for digits between 20 to 99
+                    Console.WriteLine("parsing afterwards" + input[i]);
                 }
 
             }
