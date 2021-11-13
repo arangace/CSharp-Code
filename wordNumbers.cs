dfe1b5f31aa65e6de48f34248a9d636a7ea4fc57
arangace <ahua291@aucklanduni.ac.nz>
2021-11-13 16:53:45 +1300

    conversion for cents fully works with cents identifier.

---
+++ wordNumbers.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+public class wordNumbers
+{
+    private string outputValue = "";
+
+    public static void integers()
+    {
+
+    }
+    public string concatString
+    {
+        get { return outputValue; }
+        set { outputValue = value; }
+    }
+}
